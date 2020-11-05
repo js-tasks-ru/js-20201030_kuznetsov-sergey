7f673043af5b14abffd31315a328fa54f7eaef02
Сергей Кузнецов <SerKuznetsov@croc.ru>
2020-11-05 20:17:50 +0500

    Writing the pick function

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,12 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+    const newObj = {};
+    for (let i in fields) {
+        const field = fields[i];
+        if (field in obj) {
+            newObj[field] = obj[field];
+        }
+    }
+    return newObj;
 };
