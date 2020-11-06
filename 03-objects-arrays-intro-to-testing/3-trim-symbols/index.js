8e3d01c7e04c40df277cff7f5d10da553aeaac46
Сергей Кузнецов <SerKuznetsov@croc.ru>
2020-11-07 01:55:29 +0500

    Task 3. Wirte function trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,18 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+    if (size === 0 || string === '') return '';
+    if (size === undefined) return string;
+    let out = string[0];
+    let counter = 1;
+    for (let i = 1; i < string.length; i++) {
+        if (string[i] !== string[i-1]) {
+            out += string[i];
+            counter = 1;
+        } else if (counter < size) {
+            out += string[i];
+            counter++;
+        }
+    }
+    return out;
 }
